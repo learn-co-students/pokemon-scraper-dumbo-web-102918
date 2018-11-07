8fbdf22e6bcea6791a77d91187ebfcbc827d141a
trose618 <trose618@gmail.com>
2018-11-07 09:02:28 -0500

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,14 +1,15 @@
 class Pokemon
 
-  attr_reader :id, :name, :type, :db
+  attr_reader :id, :name, :type, :db, :hp
 
   @@all = []
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, hp = nil, type:, db:)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = hp
 
     @@all << self
   end
@@ -23,8 +24,7 @@ class Pokemon
 
   def self.find(id, db)
     pokmon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
-
-    pokemon = Pokemon.new(id: pokmon[0][0], name: pokmon[0][1], type: pokmon[0][2], db: db)
+    pokemon = Pokemon.new(id: pokmon[0][0], name: pokmon[0][1], 60, type: pokmon[0][2], db: db)
     #binding.pry
   end
 
